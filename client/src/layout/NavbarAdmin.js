6ac9d6790b9f4714edd175f503055b9b96025b84
devbyanuwat <mailfordevbyanuwat@gmail.com>
2022-11-30 15:38:50 +0700

    search nav admin

--- client/src/layout/NavbarAdmin.js
+++ client/src/layout/NavbarAdmin.js
@@ -57,6 +57,7 @@ function NavbarAdmin() {
               <Nav.Link className="item-nav-admin" href="/">
                 Home
               </Nav.Link>
+
               <Nav.Link className="item-nav-admin" href="/admin/add-rice">
                 Add Rice
               </Nav.Link>
@@ -66,42 +67,17 @@ function NavbarAdmin() {
               <Nav.Link className="item-nav-admin" href="/admin/qr-scanner">
                 Scan QR
               </Nav.Link>
-
-              <Nav.Link className="item-nav-admin-logout" onClick={() => SignOut()}>
-                <span className="no-icon">Log out</span>
-              </Nav.Link>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar >
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item">
+              <Nav.Item className="item-nav-admin">
                 <div className="search-container">
-                  <div className="search-inner ">
-                    <input type="text" value={value} onChange={onChange} />
-                    <button className="btn bg-light" onClick={() => onSearch(value)}>
+                  <div className="search-inner input-group">
+                    <input placeholder="ค้นหาข้าว.." type="text" className="form-control" value={value} onChange={onChange} />
+                    <span class="input-group-text" id="basic-addon2" onClick={() => onSearch(value)}>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                       </svg>
-                    </button>
+                    </span>
                   </div>
-                  <div className="dropdown ">
+                  <div className="dropdown">
                     {result.filter(
                       item => {
                         const searchTerm = value.toLowerCase();
@@ -113,17 +89,22 @@ function NavbarAdmin() {
                         {/* <div className="fs-6">{item.RiceCategory}</div>
                        */}
                         {item.RiceCategory}
-                        <div className="dropdown-id">{item.RiceID}</div>
+                        <div className="dropdown-id text-muted">{item.RiceID}</div>
 
                       </div>
                     ))}
                   </div>
                 </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+              </Nav.Item>
+
+              <Nav.Link className="item-nav-admin-logout" onClick={() => SignOut()}>
+                <span className="no-icon">Log out</span>
+              </Nav.Link>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar >
+
 
     </div >
 
